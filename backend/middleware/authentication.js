790967180bb18e686cbcea0452a039a4adad957b
Euphorichub <106897467+euphorichubbbb@users.noreply.github.com>
2023-09-10 11:23:02 +0530

    initial backend added

---
+++ backend/middleware/authentication.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const authentication = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.passKey, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+module.exports = authentication;
